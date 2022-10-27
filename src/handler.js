01922cc649adde26162a16671a02a4cc86ef4749
denyanwardev <denyanwardev@gmail.com>
2022-10-27 16:11:12 +0800

    in-progress give feature add book

---
+++ src/handler.js
@@ -0,0 +1,23 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const id = nanoid(16);
+  const finished = false;
+  
+  if (pageCount === readPage) {
+    finished = true;
+  }
+
+};
